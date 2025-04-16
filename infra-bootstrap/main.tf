ce2c82b5f2845136279aee0391fef137d288478b
TeaganSmith <teagan.smith@my.utsa.edu>
2025-04-16 03:25:38 -0500

    Update main.tf

--- infra-bootstrap/main.tf
+++ infra-bootstrap/main.tf
@@ -51,5 +51,5 @@ resource "aws_instance" "infra-provisioner_host" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.minikube_host.public_ip
+  value = aws_instance.infra-provisioner.public_ip
 }
